527767a7eab9697256494f2fef1d34b2411200c5
keeser <1326658417@qq.com>
2023-12-18 00:04:00 +0800

    完善前端的支付代码

--- vuejs/src/components/order/UserOrder.vue
+++ vuejs/src/components/order/UserOrder.vue
@@ -73,7 +73,7 @@
               type="primary"
               size="mini"
               icon="el-icon-s-goods"
-              @click="payBy"
+              @click="payByOrderId(scope.row.order_id)"
               >支付</el-button
             >
             <el-button
@@ -129,6 +129,9 @@ export default {
         ]
       },
       cityData,
+      payForm: {
+        order_id: 0
+      },
       // 物流进度对话框
       progressDialogVisible: false,
       // 物流进度
@@ -168,6 +171,30 @@ export default {
         return this.$message.error('删除订单失败！')
       }
       this.$message.success('删除订单成功！')
+      this.getOrderList()
+    },
+    // 支付操作
+    async payByOrderId(id) {
+      const confirmResult = await this.$confirm(
+        '此操作将支付该订单, 是否继续?',
+        '提示',
+        {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }
+      ).catch((err) => err)
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('已取消支付！')
+      }
+
+      this.payForm.order_id = id
+      const { data: res } = await this.$http.post('orders/user/', this.payForm)
+      if (res.meta.status !== 200) {
+        return this.$message.error('支付订单失败！')
+      }
+      this.$message.success('支付订单成功！')
+      // 刷新
       this.getOrderList()
     },
     // 分页
