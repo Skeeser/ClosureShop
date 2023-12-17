c6c221b43bfd36beb69670ed386a0881b88b6d70
keeser <1326658417@qq.com>
2023-12-18 02:48:10 +0800

    报表支持随机颜色

--- vuejs/src/components/report/Report.vue
+++ vuejs/src/components/report/Report.vue
@@ -8,8 +8,10 @@
     </el-breadcrumb>
     <!-- 卡片视图 -->
     <el-card>
-      <!-- 2.为Echarts准备一个Dom -->
-      <div id="main" style="width: 750px; height: 400px"></div>
+      <div style="display: flex; justify-content: center; align-items: center">
+        <!-- 2.为Echarts准备一个Dom -->
+        <div id="main" style="width: 750px; height: 400px"></div>
+      </div>
     </el-card>
   </div>
 </template>
@@ -17,77 +19,82 @@
 <script>
 // 1.导入echarts
 import echarts from 'echarts'
-import _ from 'lodash'
+// import _ from 'lodash'
 
 export default {
   data() {
-    return {
-      // 需要合并的数据
-      options: {
-        title: {
-          text: '用户来源'
-        },
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'cross',
-            label: {
-              backgroundColor: '#E9EEF3'
-            }
-          }
-        },
-        grid: {
-          left: '3%',
-          right: '4%',
-          bottom: '3%',
-          containLabel: true
-        },
-        xAxis: [
-          {
-            boundaryGap: false
-          }
-        ],
-        yAxis: [
-          {
-            type: 'value'
-          }
-        ]
-      }
-    }
+    return {}
   },
   created() {},
   // 此时,页面上的元素,已经被渲染完毕了
   async mounted() {
     // 3.基于准备好的dom，初始化echarts实例
     var myChart = echarts.init(document.getElementById('main'))
-    // const { data: res } = await this.$http.get('reports/type/1')
-    // if (res.meta.status !== 200) return this.$message('获取折线图数据失败!')
+    const { data: res } = await this.$http.get('reports')
+    if (res.meta.status !== 200) return this.$message('获取折线图数据失败!')
+
+    var xData = []
+    var yData = []
+    res.data.forEach((element) => {
+      xData.push(element.goods_name)
+      yData.push(element.hot_number)
+    })
+
+    // 随机颜色数组
+    var colorList = [] // 设置颜色的数组
+    // 生成随机颜色并添加到color配置中
+    for (let i = 0; i < xData.length; i++) {
+      colorList.push(this.getRandomColor())
+    }
+    // console.log(colorList)
+
     // 4.准备数据项和配置项
     // 指定图表的配置项和数据
-    // var option = {
-    //   title: {
-    //     text: 'ECharts 入门示例'
-    //   },
-    //   tooltip: {},
-    //   legend: {
-    //     data: ['销量']
-    //   },
-    //   xAxis: {
-    //     data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-    //   },
-    //   yAxis: {},
-    //   series: [{
-    //     name: '销量',
-    //     type: 'bar',
-    //     data: [5, 20, 36, 10, 10, 20]
-    //   }]
-    // }
+    var option = {
+      title: {
+        text: '商品的热度统计'
+      },
+      tooltip: {},
+      legend: {
+        data: ['商品名称']
+      },
+      xAxis: {
+        // type: '商品名称',
+        name: '商品名称',
+        data: xData,
+        axisLabel: {
+          show: false // 将 x 轴标签隐藏
+        }
+      },
+      yAxis: {},
+      series: [
+        {
+          name: '热度',
+          type: 'bar',
+          data: yData,
+          itemStyle: {
+            normal: {
+              color: (params) => {
+                return colorList[params.dataIndex]
+              }
+            }
+          }
+        }
+      ]
+    }
+
+    console.log(xData.length)
     // 数据合并
     // const result = _.merge(res.data, this.options)
     // 5.展示数据
-    // myChart.setOption(result)
+    myChart.setOption(option)
   },
-  methods: {}
+  methods: {
+    // 生成随机颜色的函数
+    getRandomColor() {
+      return '#' + Math.floor(Math.random() * 16777215).toString(16)
+    }
+  }
 }
 </script>
 
